b8e3dd644d969f7193ac4e56cbb1ffb757f61999
pechex <pechex@users.noreply.github.com>
2025-10-24 21:05:55 +0000

    Prettified Code!

--- test.js
+++ test.js
@@ -1,14 +1,12 @@
 function someFunction() {
-    let nVar = 0;
+  let nVar = 0;
 
-    nVar++;
-    nVar--;
+  nVar++;
+  nVar--;
 
-if(nVar == 0) {
-console.log(    "Hello World!");
-} else 
-    
-    {
-console.log("Goodbye World!");
-}
+  if (nVar == 0) {
+    console.log("Hello World!");
+  } else {
+    console.log("Goodbye World!");
+  }
 }
